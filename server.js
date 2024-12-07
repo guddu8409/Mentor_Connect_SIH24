79548f0890ed43b725573f9a11740d4ddd64cba8
vivekashyap49 <vivek3112003@gmail.com>
2024-12-08 01:21:18 +0530

    quiz index done from my side

--- server.js
+++ server.js
@@ -50,7 +50,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.json());
-app.use(session(sessionConfig(process.env.MONGODB_URL, process.env.SESSION_SECRET)));
+app.use(
+  session(sessionConfig(process.env.MONGODB_URL, process.env.SESSION_SECRET))
+);
 app.use(flash());
 
 // Passport Configuration
